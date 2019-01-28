3127954eca033e7813431ec6dfd05c7426fdd18e
Dylanr57 <dylanr574423@gmail.com>
2019-01-27 21:29:48 -0800

    made scrape and db

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const db = require("./models");
 
 const app = express();
 var PORT = process.env.PORT || 8080;
@@ -14,6 +15,33 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 
 mongoose.connect(MONGODB_URI);
 
+app.get("/scrape",function(req,res){
+
+    axios.get("https://www.gamespot.com/news/").then(function(response) {
+        var $ = cheerio.load(response.data);
+
+        $("article.media.media-article").each(function(i, element) {
+            var result = {};
+
+            result.title = $(this).find("h3.media-title").text();
+            result.summary = $(this).find("p.media-deck").text();
+            result.link = $(this).find("a").attr("href");
+            result.image = $(this).find("img").attr("src");
+            db.Article.create(result)
+            .then(function(dbArticle){
+                console.log(dbArticle);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        });
+
+
+    });
+
+    res.send("Scrape Complete");
+
+});
 
 
 
